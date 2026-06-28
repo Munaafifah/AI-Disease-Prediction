edfaab8c47d64447bad49346586c785072230d4d
Munaafifah <149042750+Munaafifah@users.noreply.github.com>
2026-06-29 02:26:00 +0800

    Update settings.py

--- DiseaseAPI/settings.py
+++ DiseaseAPI/settings.py
@@ -138,3 +138,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+CSRF_TRUSTED_ORIGINS = ['https://wellcheck-mobile-iu264.ondigitalocean.app']
